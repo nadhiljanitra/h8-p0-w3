b6da329f67ff7f8cec9496c16059b047b97f62d8
nadhiljanitra <nadhiljanitraa@gmail.com>
2019-08-05 17:10:05 +0700

    Exercise-15

--- Exercise-15.js
+++ Exercise-15.js
@@ -1,24 +1,45 @@
 function groupAnimals(animals) {
-  var satuan=[]
+  var hurufDepanUnikArr=[]
   var hasil=[]
   animals.sort()
+  
   for (i=0;i<animals.length;i++){
-    satuan[i]=animals[i][0]
-  }
-  for (i=0;i<animals.length-1;i++) {
-    if (satuan[i]===satuan[i+1]){
-      hasil.pop();
-      hasil.push([animals[i],animals[i+1]])
-    } else if (satuan[i]!==satuan[i+1]){
-      hasil.push([animals[i+1]])
+    //CHECK HURUF DEPAN UNIK ENGGA
+   if(hurufDepanUnikArr.length < 1) {
+     hurufDepanUnikArr.push(animals[i][0])
+     hasil.push([animals[i]])
+   } else {
+     var check = true
+    //  var index 
+     for (j=0; j < hurufDepanUnikArr.length;j++){
+       if (hurufDepanUnikArr[j] === animals[i][0]) {
+        //  hasil[j].push([animals[i]])
+          var index = j
+         check = false
+        }
+      }
+    if(check) {
+      hurufDepanUnikArr.push(animals[i][0])
+      hasil.push([animals[i]])
+    } else {
+      hasil[index].push(animals[i])
     }
+
+
+
+   }
+
+   
   }
- return hasil; 
+  
+   console.log(hurufDepanUnikArr)
+   return hasil;
+   
 }
 
 
 // TEST CASES
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
 // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
-console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+console.log(groupAnimals(['cacing', 'ayam','asa','mokok','dadas', 'kuda', 'anoa', 'kancil', 'unta', 'cicak']));
 // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
