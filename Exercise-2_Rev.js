16d315f8b5ef42e2b8a9467f37137898bce0be17
nadhiljanitra <nadhiljanitraa@gmail.com>
2019-08-06 23:09:05 +0700

    Exercise-2_Rev

---
+++ Exercise-2_Rev.js
@@ -0,0 +1,8 @@
+function balikString(text) {
+    var isi = ''
+    for (i = text.length-1; i >=0; i--) {
+        isi += text[i] 
+    }
+    return isi;
+}
+console.log(balikString('hello world!'))
